2c1b4691b078ccfc01aab8139764398bec78fae9
prakash_28 <prakashraaj7996@gmail.com>
2023-09-15 09:50:39 -0400

    changed heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import "./App.css";
+import './App.css';
 
 function App() {
   return (
     <div className="h-full w-full">
-      <div className="font-bold flex justify-center items-center">
-        Welcome to Hank's House
+      <div className="font-bold flex flex-col justify-center items-center">
+        <h1>Welcome to Traditional Threadz</h1>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Button
+        </button>
       </div>
     </div>
   );

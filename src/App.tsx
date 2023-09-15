5fe6114ccbd88b3519fc995a2eb5a22f603f9ed4
prakash_28 <prakashraaj7996@gmail.com>
2023-09-15 09:45:02 -0400

    code set up

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="h-full w-full">
+      <div className="font-bold flex justify-center items-center">
+        Welcome to Hank's House
+      </div>
+    </div>
+  );
+}
+
+export default App;
